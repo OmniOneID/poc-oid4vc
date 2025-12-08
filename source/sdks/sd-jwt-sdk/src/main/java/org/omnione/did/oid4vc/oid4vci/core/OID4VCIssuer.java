c78a2bccb183eb1cb4fcd051573b84ad4088e8e0
shlee1223 <shlee1223@raoncorp.com>
2025-12-08 18:15:46 +0900

    feat: modify sd-jwt-sdk (support cnf jwk and header kid in sd-jwt)

--- source/sdks/sd-jwt-sdk/src/main/java/org/omnione/did/oid4vc/oid4vci/core/OID4VCIssuer.java
+++ source/sdks/sd-jwt-sdk/src/main/java/org/omnione/did/oid4vc/oid4vci/core/OID4VCIssuer.java
@@ -20,11 +20,14 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.math.BigInteger;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.security.interfaces.ECPrivateKey;
+import java.security.interfaces.ECPublicKey;
 import java.security.interfaces.RSAPrivateKey;
 import java.time.temporal.ChronoUnit;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 import org.omnione.did.oid4vc.exception.OID4VCException;
@@ -142,14 +145,37 @@ public class OID4VCIssuer {
   }
 
   private Map<String, Object> createConfirmationClaim(PublicKey holderPublicKey) {
+    Map<String, Object> jwkMap = new HashMap<>();
+    jwkMap.put("kty", getKeyType(holderPublicKey));
+    jwkMap.put("use", "sig");
 
-    return Map.of(
-        "jwk", Map.of(
-            "kty", getKeyType(holderPublicKey),
-            "use", "sig"
+    if (holderPublicKey.getAlgorithm().equals("EC")) {
+      try {
+        ECPublicKey ecPublicKey = (ECPublicKey) holderPublicKey;
 
-        )
-    );
+        String crv = getCurveNameFromECKey(ecPublicKey);
+        jwkMap.put("crv", crv);
+
+        BigInteger x = ecPublicKey.getW().getAffineX();
+        BigInteger y = ecPublicKey.getW().getAffineY();
+
+        String xBase64 = Base64.getUrlEncoder()
+            .withoutPadding()
+            .encodeToString(x.toByteArray());
+        String yBase64 = Base64.getUrlEncoder()
+            .withoutPadding()
+            .encodeToString(y.toByteArray());
+
+        jwkMap.put("x", xBase64);
+        jwkMap.put("y", yBase64);
+      } catch (Exception e) {
+        throw new IllegalArgumentException("Failed to extract EC key parameters", e);
+      }
+    } else if (holderPublicKey.getAlgorithm().equals("RSA")) {
+      // TODO
+    }
+
+    return Map.of("jwk", jwkMap);
   }
 
   private String getKeyType(PublicKey publicKey) {
@@ -159,6 +185,22 @@ public class OID4VCIssuer {
       return "EC";
     } else {
       throw new IllegalArgumentException("Unsupported key type: " + publicKey.getAlgorithm());
+    }
+  }
+
+  private String getCurveNameFromECKey(java.security.interfaces.ECPublicKey ecPublicKey) {
+    java.security.spec.ECParameterSpec ecParams = ecPublicKey.getParams();
+    int keySize = ecParams.getOrder().bitLength();
+
+    switch (keySize) {
+      case 256:
+        return "P-256";
+      case 384:
+        return "P-384";
+      case 521:
+        return "P-521";
+      default:
+        throw new IllegalArgumentException("Unsupported EC key size: " + keySize);
     }
   }
 
@@ -203,6 +245,7 @@ public class OID4VCIssuer {
       Map<String, Object> header = new HashMap<>();
       header.put("alg", algorithm);
       header.put("typ", "vc+sd-jwt");
+      header.put("kid", issuerId);
 
       Map<String, Object> payloadMap = new ObjectMapper().readValue(payloadJson,
           new TypeReference<Map<String, Object>>() {
@@ -245,6 +288,7 @@ public class OID4VCIssuer {
       Map<String, Object> header = new HashMap<>();
       header.put("alg", algorithm);
       header.put("typ", "vc+sd-jwt");
+      header.put("kid", issuerId);
 
       Map<String, Object> payloadMap = new ObjectMapper().readValue(payloadJson,
           new TypeReference<Map<String, Object>>() {
